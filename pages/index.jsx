9efa52c5ec34b9080a2f12da17f61cc146d811e7
Prashanth <prashanth@localhost>
2022-11-26 14:02:35 +0530

    Fix font

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,9 +9,9 @@ export default function IndexPage() {
           <span className="intro-top ms-lg-5">Hi! I am<br/>Prashanth C A</span>
           <p className="intro-first ms-lg-5">A freelance <b>web developer</b> from Bangalore, IN.</p>
           <p className="intro-second ms-lg-5">I specialize in UI/UX Design, Full-stack MERN/LAMP development and Mobile application development</p>
-          <div class="input-group mb-3 ms-lg-5 reach-out">
-            <input type="text" class="form-control" placeholder="Your email" aria-label="Your email" aria-describedby="button-addon2"/>
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Connect with me</button>
+          <div classname="input-group mb-3 ms-lg-5 reach-out">
+            <input type="text" classname="form-control" placeholder="Your email" aria-label="Your email" aria-describedby="button-addon2"/>
+            <button classname="btn btn-outline-secondary" type="button" id="button-addon2">Connect with me</button>
           </div>
         </div>
       </section>
